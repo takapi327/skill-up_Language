de25b047e180d7aea90f3ea4d1463a52ea6fb15c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-06 16:42:17 +0900

    Creating study models
    studyモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_073031) do
+ActiveRecord::Schema.define(version: 2020_02_06_073510) do
 
   create_table "languages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "studies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "language_id", null: false
+    t.date "day", null: false
+    t.integer "time", null: false
+    t.text "context", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["language_id"], name: "index_studies_on_language_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -31,4 +41,5 @@ ActiveRecord::Schema.define(version: 2020_02_06_073031) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "studies", "languages"
 end
