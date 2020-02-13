507f7207922f7be3e1fd755ae2425817880c5fd5
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-13 20:38:18 +0900

    Add　modal
    モーダルウィンドウの追加

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.search').on('click',function(){
+      $('.js-modal').fadeIn();
+      return false;
+  });
+  $('.js-modal-close').on('click',function(){
+      $('.js-modal').fadeOut();
+      return false;
+  });
+});
