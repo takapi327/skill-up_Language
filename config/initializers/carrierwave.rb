0b6789dcc31891c388c733f63e5dabea964fee67
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-17 17:15:02 +0900

    Update carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'skill-up-language'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
