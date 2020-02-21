5ae480743e5b30890d802263d0933bf7f68df85b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-02-21 22:59:53 +0900

    Edit index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 10 }
 
   has_many :languages
 end
