d7727fc33ca242ba731d206f81fabda152c55194
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-03-04 17:36:18 +0900

    Edit configfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '3.115.143.110', user: 'ec2-user', roles: %w{app db web}
+server '54.249.196.2', user: 'ec2-user', roles: %w{app db web}
